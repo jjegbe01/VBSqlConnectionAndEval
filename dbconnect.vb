81213c92c556aa2f4072a7fb8a557c01f4735801
Julian Egbert <jjegbe01@louisville.edu>
2016-10-14 15:41:24 -0400

    Updated for efficiency
    Optimizations made to the generated SQL query, so that it targets more specific sets of data (this was pushed to production but left out of this repo.)
    
    For any class that exposes a Dispose() function (such as Command and Connection), it's good practice to make sure that Dispose() is always called at the end of the method. If the scope of the object I instantiated is local, I should be using Using blocks for this.
    
    Modified the generated SQL query (sSQL) to use parameterized values instead of pulling them straight from the string that's pulled into the method. The criteria for this method in particular will not usually come from user input, so there's less risk. This will still help protect it from garbage data and malformed queries.
    
    Consider the use of Clone() for the sake of not affecting other threads.

--- dbconnect.vb
+++ dbconnect.vb
@@ -1,47 +1,47 @@
 Private Function FindCurrentAppCode(ByVal primaryKey As String) As String
         Dim sSQL As String = String.Empty 'Holds SQL query
         Dim TempReader As DbDataReader 'Database reader
-        Dim cmd As IDbCommand = BUDataBase.CreateNewDatabaseConnection().CreateCommand 'Executes SQL statements on datasource
         Dim tempStr As String = Nothing 'Used locally to evaluate data
         Dim retVal As String = Nothing 'Holds the string that is returned from the method
-        
-        'Initialize cmd
-        With cmd
-            .CommandType = CommandType.Text
-            .CommandTimeout = 0
-        End With
-        
-        'Generate SQL query
-        sSQL = "SELECT "
-        sSQL = sSQL & "       column_1, column_2, column_3, " & vbCrLf
-        sSQL = sSQL & "       column_4, column_5, column_6, " & vbCrLf
-        sSQL = sSQL & "       column_7, column_8, column_9 " & vbCrLf
-        sSQL = sSQL & "FROM " & Settings.RefDataLocal & ".tablename " & vbCrLf
-        sSQL = sSQL & "WHERE column_1 = '" & primaryKey & "' " & vbCrLf
-        
-        'Set cmd's command text equal to our SQL query
-        cmd.CommandText = sSQL
-        'Execute our query
-        TempReader = cmd.ExecuteReader
-        
-        '**Use TempReader to evaluate returned data**
-        '***All rows past here are optional***
-        
-        'Make sure that data exists
-        If TempReader.HasRows AndAlso TempReader.Read() Then
-            'Use a Do While to iterate through all rows returned from our query
-            Do While TempReader.Read()
-                'Set tempStr equal to a paticular value in each row
-                tempStr = TempReader("column_1")
-                'Evaluate tempStr and perform needed actions
-                If tempStr = "value1" Or tempStr = "value2" Then
-                    retVal = tempStr
-                    Exit Do    
-                End If
-            Loop
-        Else
-            retVal = ""
-        End If
-        'Return string
-        Return retVal
-End Function
+        Dim tempParam As SqlClient.SqlParameter
+		
+		'Instantiate cmd
+		Using cmd As IdbCommand = BUDataBase.CreateNewDatabaseConnection.CreateCommand
+			'Instantiate DB connection
+			Using connection as SqlClient.SqlConnection = BUDataBase.CreateNewDatabaseConnection
+				'Fill out tempParam details; [Name, Datatype, and size]
+				tempParam = New SqlClient.SqlParameter("@ParameterOne", SqlDbType.VarChar, 10)
+				'Set tempParam value equal to the value being passed into the method
+				tempParam.Value = primaryKey
+				'Set properties for cmd, *don't forget to add tempParam as a usable parameter*
+				With cmd
+					.CommandType = CommandType.Text
+					.CommandTimeout = 0
+					.Connection = connection
+					.Parameters.Add(tempParam)
+				End With
+				
+				'Generate SQL query
+				sSQL = "SELECT "
+				sSQL = sSQL & "       column_1, column_2, column_3, " & vbCrLf
+				sSQL = sSQL & "       column_4, column_5, column_6, " & vbCrLf
+				sSQL = sSQL & "       column_7, column_8, column_9 " & vbCrLf
+				sSQL = sSQL & "FROM " & Settings.RefDataLocal & ".tablename " & " with(nolock)" & vbCrLf
+				sSQL = sSQL & "WHERE column_1 = @ParameterOne" & vbCrLf
+				'This last line is optional
+				sSQL = sSQL & "AND column_3 in ('X', 'Y')"
+				
+				cmd.CommandText = sSQL
+				TempReader = cmd.ExecuteReader
+				If TempReader.Read() AndAlso TempReader.HasRows Then
+					tempStr = TempReader("column_3")
+					If (tempStr = "X" OrElse tempStr = "Y") Then
+						retVal = tempStr
+					End If
+				End If
+				connection.Close()
+				TempReader.Close()
+				Return retVal
+			End Using
+		End Using
+	End Function
